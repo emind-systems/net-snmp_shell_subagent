c31b900d50bc25859449e0de84b345fe2a4f8cb0
Lahav Savir <lahavs@emind.co>
2013-04-29 08:41:27 +0300

    Initial publish

---
+++ snmp_subagent.sh
@@ -0,0 +1,153 @@
+#!/bin/sh -f
+
+# $Rev:: 896           $
+# $Author:: lahavs     $
+# $Date:: 2009-12-12 0#$
+
+input_param=$*
+
+# Configurations
+base_dir=/usr/local/emind/snmp-subagent
+cfg_file=${base_dir}/snmp_subagent.cfg
+
+function init ()
+{
+    write_log "==== Generic SubAgent Started ===="
+    write_log "input_param=${input_param}"	
+}
+
+function find_oid ()
+{    
+    line_num=""
+    line_num=$(grep -n "${req_oid}:" ${cfg_file} | cut -f1 -d:)
+    
+    if [ "${line_num}" = "" ]; then
+        write_log "find_oid oid: ${req_oid} is not found in ${cfg_file}"
+	return
+    else
+	write_log "find_oid oid: ${req_oid} found in:${cfg_file} line:${line_num}"
+    fi
+    
+	if [ "${mode}" == "GETNEXT" ]; then
+	
+		# check if snmpd was called by snmpgetnext command
+        # set total_lines to check if we get the last oid
+        total_lines=$(wc -l ${cfg_file} | cut -f1 -d" ")
+
+        # check if we get the last oid, else getting the next oid
+        if [ ${line_num} -lt ${total_lines} ]; then
+		line_num=$(( ${line_num} + 1 ))
+	else [ ${line_num} -eq ${total_lines} ];
+		line_num=0
+	fi
+    fi
+}
+
+function parse_line ()
+{
+	line=$1
+    file=$2
+	line_data=$(tail -n +${line} ${file} | head -1)
+	
+	oid=$(echo ${line_data} | cut -f1 -d:)
+	write_log "parse_line oid=${oid}"
+	
+	type=$(echo ${line_data} | cut -f2 -d:)
+	write_log "parse_line type=${type}"
+	
+	cmd=$(echo ${line_data} | cut -f3 -d:)
+	write_log "parse_line cmd=${cmd}"
+}
+
+function write_log()
+{
+	logger -t snmp-subsgent[$$] -- "$*"
+}
+
+function exec_cmd ()
+{
+	command=$*
+	write_log "Exceuting ${cmd}"
+	data=$(${cmd})
+	#data=`exec ${cmd}`
+	err_code=$?
+}
+
+function on_error ()
+{
+	my_oid=$1
+	error_code=$2
+	error_desc=$3
+	write_log "error_code=${error_code} error_desc=${error_desc}"
+	# see spec for error handling, for returning no_such by master
+	# return_results_to_master ${my_oid} STRING ${error_code}
+	finish
+}
+
+function return_results_to_master ()
+{
+        my_oid=$1
+        my_type=$2
+        my_value=$3
+        
+        write_log "--- RETRUN RESULTS ---"
+        write_log "oid=${my_oid}"
+        write_log "type=${my_type}"
+        write_log "value=${my_value}"
+
+        echo ${my_oid}
+        echo ${my_type}
+        echo ${my_value}
+}
+
+function finish ()
+{
+        write_log "==== Generic SubAgent Ended ===="
+        exit 0
+}
+
+mode=""
+req_oid=""
+
+init
+while getopts g:s:n: OPTNAME
+do	
+	case "$OPTNAME" in
+	g)
+		mode=GET
+		req_oid="$OPTARG"
+		;;
+	s)	
+		mode=SET
+		req_oid="$OPTARG"
+	;;
+	n)
+		mode=GETNEXT
+		req_oid="$OPTARG"
+	;;
+	esac
+done
+
+if [ "${mode}" = "" ] || [ "${req_oid}" = "" ]; then
+	on_error 0 -2 "Bad Input"
+fi
+
+write_log "mode=${mode} req_oid=${req_oid}"
+
+find_oid									#sets: line_num 
+if [ "${line_num}" = 0 ]; then
+	on_error ${req_oid} -2 "No NEXT oid found"
+elif [ "${line_num}" != "" ]; then
+	 parse_line ${line_num} ${cfg_file}		#sets oid, type, cmd
+else
+	on_error ${req_oid} -1 "OID Not Found"      
+fi
+
+exec_cmd ${cmd}   					        #sets: err_code, data
+if [ ${err_code} ]; then
+	return_results_to_master ${oid} ${type} ${data}
+else
+	on_error ${oid} ${err_code} "${cmd} failed"
+fi
+
+finish
