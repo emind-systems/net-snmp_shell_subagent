4649fde22daf27d00e992a8e0678560f10eacc8c
lahavsavir <lahavs@emind.co>
2013-04-30 06:35:41 +0300

    Update snmp_subagent.sh

--- snmp_subagent.sh
+++ snmp_subagent.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -f
 
-# $Rev:: 896           $
-# $Author:: lahavs     $
-# $Date:: 2009-12-12 0#$
+# --------- License Info ---------
+# Copyright 2013 Emind Systems Ltd - htttp://www.emind.co
+# This file is part of Emind Systems DevOps Tool set.
+# Emind Systems DevOps Tool set is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Emind Systems DevOps Tool set is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Emind Systems DevOps Tool set. If not, see http://www.gnu.org/licenses/.
 
 input_param=$*
 
